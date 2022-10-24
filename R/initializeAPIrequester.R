7d9c9c640cdc17180fbf268502135cb2f42b84d0
saxasm <97365103+saxasm@users.noreply.github.com>
2022-10-24 19:12:02 +0200

    initializeAPIrequester can init from JSON object
    This will be useful, since we can put this JSON object as a string in our user environment/the repo secrets, and so we can have it initialize without needing to have keys written to file.

--- R/initializeAPIrequester.R
+++ R/initializeAPIrequester.R
@@ -6,11 +6,31 @@
 #'
 #' @param appName The name of your app
 #' @param apiKeys The API keys to be used for the pooling of API keys in apiRequest
+#' @param apiKeyNames Names for the API keys, that will be displayed in error messages. Should normally be the name of the owner of the key. Optional, but very useful for error messages.
+#' @param JSONinitData A string that can be parsed by the jsonlite-package JSON reader. Could be actual JSON in a string, or the path to a file or a URL from which to read it. Note that this format requires names for the API keys.
 #'
 #' @return TRUE, if initialization was successful.
 #' @export
 
-initializeAPIrequester <- function(appName, apiKeys) {
+initializeAPIrequester <- function(appName = NULL, apiKeys = NULL, apiKeyNames = NULL, JSONinitData = NULL) {
+  # We start by determining if we are getting our data from the JSON or from the parameters of our function.
+  # If we get both, the JSON takes precedence.
+  if (!missing(JSONinitData)) {
+    # Try to read the JSON, and verify that it has the right structure:
+    jsonData <- jsonlite::fromJSON(JSONinitData)
+    if (!identical(names(jsonData), c("appName","keys")) || !is.list(jsonData$keys)) {
+      stop("Invalid structure of supplied JSON data.")
+    }
+
+    # Having read the JSON, we move the data from it into the other arguments, and then proceed as if
+    # those had been provided:
+    appName <- jsonData$appName
+    apiKeys <- unlist(jsonData$keys)
+    apiKeyNames <- names(jsonData$keys)
+  } else if (missing(appName) || missing(apiKeys)) {
+    # If we did not get any JSON data, we need to check that the other data we need is present.
+    stop("Called initializeAPIrequester with neither JSON data nor data in arguments.")
+  }
   # Start by checking if the apiRequesterData environment already exists. If
   # it does, we will update its contents, if not, we start by creating it.
   if (!exists(".apiRequesterData")) {
