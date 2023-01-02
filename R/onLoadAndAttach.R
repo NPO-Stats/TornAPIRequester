c9f5b8d1f68a58d43a4f86801e56f084e252237f
saxasm <saxasm@gmail.com>
2023-01-02 23:42:39 +0100

    Better load and attach practices
    Ideally onLoad should be as silent as possible - so we only message about initializing if we actually did it in onLoad, and give the reminder in onAttach instead.

--- R/onLoadAndAttach.R
+++ R/onLoadAndAttach.R
@@ -2,7 +2,11 @@
   if ("TAPIR_INIT" %in% names(Sys.getenv())) {
     initializeAPIrequester()
     packageStartupMessage("Loaded TornAPIRequester and initialized using TAPIR_INIT environment variable.")
-  } else {
+  }
+}
+
+.onAttach <- function(libname, pkgname) {
+  if (!("TAPIR_INIT" %in% names(Sys.getenv()))) {
     packageStartupMessage("Loaded TornAPIRequester - remember to initialize with initalizeAPIrequester() before trying to make requests")
   }
 }
